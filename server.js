53b4bb43082f162b457b1562f40fe28c4e14c6ee
rishabh kalra <rishabh.kalra@niit.com>
2020-03-31 18:35:54 +0530

    served dist folder and connected keyclock

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const Keycloak = require('keycloak-connect');
 const session = require('express-session');
 const express = require('express')
+const path = require('path');
 
 // creating session objec to store user sessions in cookie
 var memoryStore = new session.MemoryStore();
-// var keycloak = new Keycloak({ store: memoryStore });
+var keycloak = new Keycloak({ store: memoryStore });
 
 
 // initilaize keycloak with express session storage
@@ -23,6 +24,9 @@ app.use(session({
 // add keycloak as a middlewaee to the app
 app.use(keycloak.middleware());
 
+// serve the static files
+app.use(keycloak.protect(), express.static(path.join(__dirname, '/client/dist/Neighbourhood/')));
+
 // protect test route with the keycloak
 app.get('/test', keycloak.protect(),  (req, res) =>  {
     res.send("OK")
@@ -33,10 +37,10 @@ app.get('/special', keycloak.protect('realm:special'),  (req, res) =>  {
     res.send("OK")
 });
 
-
-app.get('/', (req, res) => [
-    res.send("OK")
-])
+// login url
+app.get('/login', keycloak.protect(),  (req, res) =>  {
+    res.redirect('http://localhost:3000/');
+});
 
 // add keycloak as middleware with logout configuration to delete user sessions on logout
 app.use('/logout', keycloak.middleware({ logout: '/' }));
@@ -44,4 +48,5 @@ app.use('/logout', keycloak.middleware({ logout: '/' }));
 
 app.listen(3000, function () {
     console.log('Listening at http://localhost:3000');
+    console.log('serving angular app from -> ', path.join(__dirname, '/client/dist/Neighbourhood/'));
 });
